d495d9eb33ee52d49b4a4bbc5aca10f974e782bc
channyeintun404 <channyeintun404@gmail.com>
2021-08-12 21:11:42 +0630

    add & delete option color and size in edit page, design of link page

--- src/app/pages/product-edit/product-edit.component.ts
+++ src/app/pages/product-edit/product-edit.component.ts
@@ -7,7 +7,7 @@ import { Option } from 'src/app/models/option.model';
 import { Variant } from 'src/app/models/variants.model';
 
 import { ProductDetailsComponent } from '../product-details/product-details.component';
-
+import axios from 'axios';
 
 @Component({
   selector: 'app-product-edit',
@@ -19,6 +19,7 @@ export class ProductEditComponent implements OnInit {
 
   steps: any = [];
   cards: any = [];
+  checks: any= [];
 
   @Input() id: number;
   name: String;
@@ -38,19 +39,27 @@ export class ProductEditComponent implements OnInit {
 
    //option
    options : Option[];
+   addOptionSize : boolean;
+   addOptionColor : boolean
    addOption: boolean;
    isOption: boolean;
    optionId: any;
-   optionPostion :  string="";
-   optionName : string="";
-   optionStatus: string="";
-   variantIdArray: any[];
-   variantnameArray: any[];
-   variantPositionArray: any[];
-   variantStatusArray: any [];
-   variant : Variant[];
+   optionSizePostion :  string="";
+   optionSizeName : string="";
+   optionSizeStatus: string="";
+   optionColorPostion :  string="";
+   optionColorName : string="";
+   optionColorStatus: string="";
+  //  variant : Variant[];
+   variantSize : Variant[];
+   variants : any[];
+   optionSizeId: any;
+   optionColorId: any;
+   variants_size: any[];
+   variants_color: any[];
+   optionIdArray: any[]=[];
 
-   imagearray: any[]=[];
+   imagesUrlArray: any[]=[];
    addImage : boolean;
 
    //uploadimage and update image
@@ -59,6 +68,15 @@ export class ProductEditComponent implements OnInit {
    addImageRow:boolean;
    imagesArrayforView: any[]=[];   
   imagepairdetailed: any[]=[];
+  selectedFile: any;
+  variantSizeIdArray: any[];
+  variantSizeNameArray: any[];
+  variantSizePositionArray: any[];
+  variantSizeStatusArray: any[];
+  variantColorIdArray: any[];
+  variantColorNameArray: any[];
+  variantColorPositionArray: any[];
+  variantColorStatusArray: any[];
 
 
   constructor(public modalController: ModalController,    
@@ -71,6 +89,7 @@ export class ProductEditComponent implements OnInit {
 
     this.getProductById();
     this.getProductOptions();
+    // this.getProductColorOptions();
 
   // Checkout steps   
   this.steps = [
@@ -88,6 +107,23 @@ export class ProductEditComponent implements OnInit {
     }
   ]
 
+this.checks = [
+
+  {
+    step: "Detail",
+    isSelected: true
+  },
+  {
+    step: "Images",
+    isSelected: false
+  },
+  {
+    step: "Confirm",
+    isSelected: false
+  }
+
+]
+
   // Payment cards images
   this.cards = ["assets/images/cards/visa.png",
     "assets/images/cards/mastercard.png",
@@ -101,16 +137,23 @@ export class ProductEditComponent implements OnInit {
   if (this.steps[0].isSelected) {
     this.steps[0].isSelected = false;
     this.steps[1].isSelected = true;
+    this.checks[0].isSelected = true;
+    this.checks[1].isSelected = true;
     this.saveChanges();
   }
   // If current section is Billing then next section confirm will be visible 
   else if (this.steps[1].isSelected) {
-    this.steps[0].isSelected = false;
+     this.steps[0].isSelected = false;
     this.steps[1].isSelected = false;
-    this.steps[2].isSelected = true;
-    // this.saveChangesProductImage();
+     this.steps[2].isSelected = true;
+     this.checks[0].isSelected = true;
+    this.checks[1].isSelected = true;
+    this.checks[2].isSelected = true;
+    this.saveChangesProductImage();
   }
 }
+
+
 
 getProductById(){
   console.log("id is ="+this.id)
@@ -125,45 +168,61 @@ getProductById(){
     this.status = res['status']
     for (const image of Object.values(res['image_pairs'])){
       // console.log(image['detailed']['http_image_path'])
-      this.imagearray.push(image['detailed']['http_image_path'])
+      this.imagesUrlArray.push(image['detailed']['http_image_path'])
     }
-    console.log(this.imagearray)
+    console.log(this.imagesUrlArray)
   })
 }
 
 //getproductOption
 getProductOptions(){
-    
   this.optionsService.getProductsOptions(this.id).then((resp: any) => {
 console.log(Object.values(resp))
     for (const variants of Object.values(resp)) {
-      console.log(variants['option_id']) 
-      this.optionId=variants['option_id']
-      this.getOptionsById(variants['option_id']);     
+      // console.log(variants['option_id']) 
+      this.optionIdArray.push(variants['option_id'])
+           
     }
-   
+    console.log(this.optionIdArray)
+    this.optionSizeId = this.optionIdArray[0]
+    this.optionColorId = this.optionIdArray[1]
+      this.getOptionsSizeById(this.optionSizeId);
+      this.getOptionsColorById(this.optionColorId);
+  //  this.getProductColorOptions();
   })
   
 }
-
-getOptionsById(option_id){
+// getProductColorOptions(){
+    
+//   this.optionsService.getProductsOptions(this.id).then((resp: any) => {
+// console.log(Object.values(resp))
+//     for (const variant of Object.values(resp)) {
+//       console.log(variant['option_id']) 
+//       this.optionColorId=variant['option_id']
+//       this.getOptionsColorById(this.optionColorId);     
+//     }
+   
+//   })
+  
+// }
+getOptionsSizeById(option_id){
   this.optionsService.getOptionsById(option_id).then((res: any) => {
     this.options=[];
     
-    this.variant=[]
-    this.variantIdArray=[]
-    this.variantnameArray=[]
-    this.variantPositionArray=[]
-    this.variantStatusArray=[]
+    this.variants_size=[]
+    this.variantSizeIdArray=[]
+    this.variantSizeNameArray=[]
+    this.variantSizePositionArray=[]
+    this.variantSizeStatusArray=[]
     for (const variant of Object.values(res['variants'])) {
         // imagesArr.push(img['detailed']['image_path']);
         // console.log("variant are "+variant['variant_name'])
 
-        this.variantnameArray.push(variant['variant_name'])
-        this.variantPositionArray.push(variant['position'])
-        this.variantStatusArray.push(variant['status'])
-        this.variantIdArray.push(variant['variant_id'])
-        console.log("variant are "+this.variantnameArray)
+        this.variantSizeNameArray.push(variant['variant_name'])
+        this.variantSizePositionArray.push(variant['position'])
+        this.variantSizeStatusArray.push(variant['status'])
+        this.variantSizeIdArray.push(variant['variant_id'])
+        console.log("variant are "+this.variantSizeNameArray)
       }  
 
     // console.log("this getopiton res"+JSON.stringify(resp));
@@ -171,43 +230,92 @@ getOptionsById(option_id){
         option_id : parseInt( res['option_id']),
         option_name:  res['option_name'],
         product_id:parseInt( res['product_id']),
-        variant_id:this.variantIdArray,
-        position: this.variantPositionArray,
-        status:this.variantStatusArray,
-        variant_name:this.variantnameArray
+        variant_id:this.variantSizeIdArray,
+        position: this.variantSizePositionArray,
+        status:this.variantSizeStatusArray,
+        variant_name:this.variantSizeNameArray
     })
 
-    for(var i =0; i<this.variantnameArray.length; i++){
-      this.variant.push({
-        "id":this.variantIdArray[i],
-        "variant_name":this.variantnameArray[i],
-        "position":this.variantPositionArray[i],
-        "status": this.variantStatusArray[i]
+    for(var i =0; i<this.variantSizeNameArray.length; i++){
+      this.variants_size.push({
+        "id":this.variantSizeIdArray[i],
+        "variant_name":this.variantSizeNameArray[i],
+        "position":this.variantSizePositionArray[i],
+        "status": this.variantSizeStatusArray[i]
       })  
     }  
+    console.log(this.variants_size)
+    // console.log("this get opiton is "+JSON.stringify(this.options))
+    // console.log("variants name are "+JSON.stringify(this.variantnameArray))
+  })
+}  
+
+getOptionsColorById(option_id){
+  this.optionsService.getOptionsById(option_id).then((res: any) => {
+    // this.options=[];
+    
+    this.variants_color=[]
+    this.variantColorIdArray=[]
+    this.variantColorNameArray=[]
+    this.variantColorPositionArray=[]
+    this.variantColorStatusArray=[]
+    for (const variant of Object.values(res['variants'])) {
+        // imagesArr.push(img['detailed']['image_path']);
+        // console.log("variant are "+variant['variant_name'])
+
+        this.variantColorNameArray.push(variant['variant_name'])
+        this.variantColorPositionArray.push(variant['position'])
+        this.variantColorStatusArray.push(variant['status'])
+        this.variantColorIdArray.push(variant['variant_id'])
+        console.log("variant are "+this.variantColorNameArray)
+      }  
+
+    // console.log("this getopiton res"+JSON.stringify(resp));
+    // this.options.push({
+    //     option_id : parseInt( res['option_id']),
+    //     option_name:  res['option_name'],
+    //     product_id:parseInt( res['product_id']),
+    //     variant_id:this.variantIdArray,
+    //     position: this.variantPositionArray,
+    //     status:this.variantStatusArray,
+    //     variant_name:this.variantnameArray
+    // })
+
+    for(var i =0; i<this.variantColorNameArray.length; i++){
+      this.variants_color.push({
+        "id":this.variantColorIdArray[i],
+        "variant_name":this.variantColorNameArray[i],
+        "position":this.variantColorPositionArray[i],
+        "status": this.variantColorStatusArray[i]
+      })  
+    }  
+    console.log(this.variants_color)
     // console.log("this get opiton is "+JSON.stringify(this.options))
     // console.log("variants name are "+JSON.stringify(this.variantnameArray))
   })
 }  
 
       //add and update option
-      createOption(){
-        if(this.optionName!=""){
-        this.variantnameArray.push(this.optionName)
-        this.variantPositionArray.push(this.optionPostion)
-        this.variantStatusArray.push(this.optionStatus)
+       createSizeOption(){
+        if(this.optionSizeName!=""){
+        this.variantSizeNameArray.push(this.optionSizeName)
+        this.variantSizePositionArray.push(this.optionSizePostion)
+        this.variantSizeStatusArray.push(this.optionSizeStatus)
       }
+
         // console.log(this.variantnameArray)
-        this.variant=[];
-        for(var i =0; i<this.variantnameArray.length; i++){
-          this.variant.push({
+        // this.variantSize=[];
+        const variantSizeArray =[];
+        for(var i =0; i<this.variantSizeNameArray.length; i++){
+          variantSizeArray.push({
             "id":i,
-            "variant_name":this.variantnameArray[i],
-            "position":this.variantPositionArray[i],
-            "status": this.variantStatusArray[i]
+            "variant_name":this.variantSizeNameArray[i],
+            "position":this.variantSizePositionArray[i],
+            "status": this.variantSizeStatusArray[i]
           })
         }
-        console.log("******"+JSON.stringify(this.variant))
+        console.log("*****",variantSizeArray)
+        // console.log("******"+JSON.stringify(this.variant))
         const convertArrayToObject = (array, key) => {
           const initialValue = {};
           return array.reduce((obj, item) => {
@@ -217,41 +325,84 @@ getOptionsById(option_id){
             };
           }, initialValue);
         };
-        console.log(convertArrayToObject( this.variant,'id'))
+        console.log(convertArrayToObject( variantSizeArray,'id'))
 
         // console.log( Object.assign({},Object.assign({},this.variantnameArray)))
-        this.optionsService.updateOptions(this.optionId,{
-          "variants": this.variant
+        this.optionsService.updateOptions(this.optionSizeId,{
+          "variants": variantSizeArray
         })
-        this.addOption=false
-        this.optionName=""
-        this.optionPostion=""
-        this.optionStatus=""
+        this.addOptionSize=false
+        this.optionSizeName=""
+        this.optionSizePostion=""
+        this.optionSizeStatus=""
+        this.getProductOptions();
+      }
+
+      createColorOption(){
+        if(this.optionColorName!=""){
+        this.variantColorNameArray.push(this.optionColorName)
+        this.variantColorPositionArray.push(this.optionColorPostion)
+        this.variantColorStatusArray.push(this.optionColorStatus)
+      }
+
+        // console.log(this.variantnameArray)
+        // this.variantSize=[];
+        const variantColorArray =[];
+        for(var i =0; i<this.variantColorNameArray.length; i++){
+          variantColorArray.push({
+            "id":i,
+            "variant_name":this.variantColorNameArray[i],
+            "position":this.variantColorPositionArray[i],
+            "status": this.variantColorStatusArray[i]
+          })
+        }
+        console.log("*****",variantColorArray)
+        // console.log("******"+JSON.stringify(this.variant))
+        const convertArrayToObject = (array, key) => {
+          const initialValue = {};
+          return array.reduce((obj, item) => {
+            return {
+              ...obj,
+              [item[key]]: item,
+            };
+          }, initialValue);
+        };
+        console.log(convertArrayToObject( variantColorArray,'id'))
+
+        // console.log( Object.assign({},Object.assign({},this.variantnameArray)))
+        this.optionsService.updateOptions(this.optionColorId,{
+          "variants": variantColorArray
+        })
+        this.addOptionColor=false
+        this.optionColorName=""
+        this.optionColorPostion=""
+        this.optionColorStatus=""
+        this.getProductOptions();
       }
 
 
-
 //update option
-updateOption(){
-  if(this.optionName!=""){
-  this.variantnameArray.push(this.optionName)
-  this.variantPositionArray.push(this.optionPostion)
-  this.variantStatusArray.push(this.optionStatus)
+updateOptionSize(position){
+// delete function
+for(var i=0;i<this.variants_size.length;i++){
+  if(this.variants_size[i].position==position)   
+  this.variants_size.splice(i,1)
 }
-  console.log(this.variant)
+
+  console.log(this.variants_size)
   // this.variant=[];
   const namearray=[];
   const positionarray=[];
   const statusarray=[];
-  for(var i =0; i<this.variant.length; i++){
-    namearray.push(this.variant[i].variant_name);
-    positionarray.push(this.variant[i].position);
-    statusarray.push(this.variant[i].status);
+  for(var i =0; i<this.variants_size.length; i++){
+    namearray.push(this.variants_size[i].variant_name);
+    positionarray.push(this.variants_size[i].position);
+    statusarray.push(this.variants_size[i].status);
   }
   console.log(namearray)
 
   const variantsarray=[]
-  for(var i =0; i<this.variant.length; i++){
+  for(var i =0; i<this.variants_size.length; i++){
     variantsarray.push({
       "id":i,
       "variant_name":namearray[i],
@@ -273,18 +424,108 @@ updateOption(){
   console.log(convertArrayToObject( variantsarray,'id'))
 
   // console.log( Object.assign({},Object.assign({},this.variantnameArray)))
-  this.optionsService.updateOptions(this.optionId,{
+  
+  
+  this.optionsService.updateOptions(this.optionSizeId,{
     "variants": variantsarray
   })
 
 }
 
- //open and closez
- openAddRow(){        
-  this.addOption=true;         
+updateOptionColor(position){
+  // delete function
+  for(var i=0;i<this.variants_color.length;i++){
+    if(this.variants_color[i].position==position)   
+    this.variants_color.splice(i,1)
+  }
+  
+    console.log(this.variants_color)
+    // this.variant=[];
+    const namearray=[];
+    const positionarray=[];
+    const statusarray=[];
+    for(var i =0; i<this.variants_color.length; i++){
+      namearray.push(this.variants_color[i].variant_name);
+      positionarray.push(this.variants_color[i].position);
+      statusarray.push(this.variants_color[i].status);
+    }
+    console.log(namearray)
+  
+    const variantsarray=[]
+    for(var i =0; i<this.variants_color.length; i++){
+      variantsarray.push({
+        "id":i,
+        "variant_name":namearray[i],
+        "position":positionarray[i],
+        "status": statusarray[i]
+      })
+    }
+    console.log(variantsarray)
+    // console.log("******"+JSON.stringify(this.variant))
+    const convertArrayToObject = (array, key) => {
+      const initialValue = {};
+      return array.reduce((obj, item) => {
+        return {
+          ...obj,
+          [item[key]]: item,
+        };
+      }, initialValue);
+    };
+    console.log(convertArrayToObject( variantsarray,'id'))
+  
+    // console.log( Object.assign({},Object.assign({},this.variantnameArray)))
+    
+    
+    this.optionsService.updateOptions(this.optionColorId,{
+      "variants": variantsarray
+    })
+  
+  }
+
+
+onFileSelected(event){
+
+  this.selectedFile = <File> event.target.files[0];
+  
+  console.log(this.selectedFile)
 }
-closeAddRow(){
-  this.addOption=false;
+
+//upload images
+onUpload(){
+  const fd = new FormData();
+  fd.append('file',this.selectedFile)
+  fd.append("upload_preset", "my-preset"); 
+
+  axios({
+    url:'https://api.cloudinary.com/v1_1/u1textile/image/upload',
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },data:fd
+  }).then((res: any) => {
+      console.log(res)
+      // this.mainImagesURl= res.data.url
+      this.imagesUrlArray.push(res.data.url)        
+    }).catch(function(err){
+          console.error(err)
+        });   
+this.addImage=true         
+}    
+
+ //open and closez
+ openAddSizeRow(){ 
+  this.addOptionSize  =true;     
+  // this.addOption=true;         
+}
+openAddColorRow(){ 
+  this.addOptionColor  =true;     
+  // this.addOption=true;         
+}
+closeAddSizeRow(){
+  this.addOptionSize=false;
+}
+closeAddColorRow(){
+  this.addOptionColor=false;
 }
 
 saveChanges(){
@@ -325,26 +566,26 @@ addproduct() {
 }
 
 saveChangesProductImage(){
-  for(var i =0; i<this.imagearray.length; i++){
-    if(i==0){
-      this.mainImagesURl=this.imagearray[0];
+  for(var i =0; i<this.imagesUrlArray.length; i++){
+    if(i==this.imagesUrlArray.length-1){
+      this.mainImagesURl=this.imagesUrlArray[this.imagesUrlArray.length-1];
     
     }
-    const imageurlarray=[]
-    imageurlarray.push({
-      "image_path":this.imagearray[i],
-      "http_image_path":this.imagearray[i],
-      "https_image_path":this.imagearray[i]
-    })
+    // const imageurlarray=[]
+    // imageurlarray.push({
+    //   "image_path":this.imagesUrlArray[i],
+    //   "http_image_path":this.imagesUrlArray[i],
+    //   "https_image_path":this.imagesUrlArray[i]
+    // })
 
     // const result = imageurlarray.reduce((obj, cur) => ({...obj, [cur.sid]: cur}), {})
     // console.log("result"+JSON.stringify(result))
     this.imagepairdetailed.push({
       "id":i,
       "detailed":{
-        "image_path":this.imagearray[i],
-        "http_image_path":this.imagearray[i],
-        "https_image_path":this.imagearray[i]            
+        "image_path":this.imagesUrlArray[i],
+        "http_image_path":this.imagesUrlArray[i],
+        "https_image_path":this.imagesUrlArray[i]            
       }           
       
     })
@@ -386,12 +627,12 @@ saveChangesProductImage(){
 
 deleteimage(image){
   console.log(image)
-  for(var i=0;i<this.imagearray.length;i++){
-    if(this.imagearray[i]==image)
-    // delete this.imagearray[i]    
-    this.imagearray.splice(i,1)
+  for(var i=0;i<this.imagesUrlArray.length;i++){
+    if(this.imagesUrlArray[i]==image)
+    // delete this.imagesUrlArray[i]    
+    this.imagesUrlArray.splice(i,1)
   }
-// console.log(this.imagearray.length)
+// console.log(this.imagesUrlArray.length)
 }
 addingImage(){
 this.addImage=true;
