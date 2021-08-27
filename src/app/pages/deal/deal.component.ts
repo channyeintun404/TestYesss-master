425a82d0bc74ee895d159a90ba94f4a1e77b103e
channyeintun404 <channyeintun404@gmail.com>
2021-08-27 21:51:10 +0630

    design of add product page and design of deal page,table design of edit page and detail page

--- src/app/pages/deal/deal.component.ts
+++ src/app/pages/deal/deal.component.ts
@@ -4,11 +4,13 @@
  * @copyright Copyright (c) 2020
  * @license   AppsPlaces
  */
-
-import { Component, OnInit } from '@angular/core';
+ import {IonContent} from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DealsService } from '../../services/deals.service';
 import { CartComponent } from '../cart/cart.component';
 import { ModalController } from '@ionic/angular';
+import { CheckoutComponent } from '../checkout/checkout.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-deal',
@@ -16,11 +18,13 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./deal.component.scss'],
 })
 export class DealComponent implements OnInit {
-
+  // @ViewChild('scrollElement') content: IonContent;
+  @ViewChild(IonContent, { static: false }) content: IonContent;
   deals: any = [];
 
   constructor(private dealsService: DealsService,
-    private modalController: ModalController) { }
+    private modalController: ModalController,
+    private router: Router) { }
 
   ngOnInit() {
     this.getDeals();
@@ -38,5 +42,40 @@ export class DealComponent implements OnInit {
     });
     return await modal.present();
   }
+  // go to checkout page function
+  async openCheckoutPage() {
+    const modal = await this.modalController.create({
+      component: CheckoutComponent
+    });
+    return await modal.present();
+  }
+  // Go to order page function
+  openOrdersPage() {
+    this.router.navigate(['/tabs/orders']);
+  }
+  // Go to products page function
+   openProductsPage() {
+    this.router.navigate(['/tabs/products']);
+  }
+  // Go to category page function
+  openCategoryPage() {
+    this.router.navigate(['/tabs/categories']);
+  }
 
+  // async openCampaignPage() {
+  //   const modal = await this.modalController.create({
+  //     component: DealComponent
+  //   });
+  //   return await modal.present();
+  // }
+// Go to campaign page function
+  ScrollToBottom() {
+      this.content.scrollToBottom(100);
+    }
+  // Back to previous screen
+  dismiss() {
+    this.modalController.dismiss({
+      'dismissed': true
+    })
+  }
 }
